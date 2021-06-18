1845e05865b6866982b13f2efaa7875d8d383598
jacquelineb <jacquelinesotheremail@gmail.com>
2021-06-17 18:05:03 -0700

    Moved code for switching contents into a single function

--- src/index.js
+++ src/index.js
@@ -8,43 +8,42 @@ loadHomePage();
 function createNavBar() {
   const nav = document.createElement('nav');
   const homeLink = document.createElement('a');
+  homeLink.setAttribute('data-name', 'home-content');
   homeLink.textContent = 'HOME';
   homeLink.href = '#';
-  homeLink.addEventListener('click', () => {
-    const homeContent = document.getElementById('home-content');
-    const currContent = document.getElementById('content').firstChild;
-    if (currContent !== homeContent) {
-      currContent.remove();
-      loadHomePage();
-    }
-  });
+  homeLink.addEventListener('click', switchContents);
   nav.appendChild(homeLink);
 
   const menuLink = document.createElement('a');
+  menuLink.setAttribute('data-name', 'menu-content');
   menuLink.textContent = 'MENU';
   menuLink.href = '#';
-  menuLink.addEventListener('click', () => {
-    const menuContent = document.getElementById('menu-content');
-    const currContent = document.getElementById('content').firstChild;
-    if (currContent !== menuContent) {
-      currContent.remove();
-      loadMenuPage();
-    }
-  });
+  menuLink.addEventListener('click', switchContents);
   nav.appendChild(menuLink);
 
   const contactLink = document.createElement('a');
+  contactLink.setAttribute('data-name', 'contact-content');
   contactLink.textContent = 'CONTACT';
   contactLink.href = '#';
-  contactLink.addEventListener('click', () => {
-    const contactContent = document.getElementById('contact-content');
-    const currContent = document.getElementById('content').firstChild;
-    if (currContent !== contactContent) {
-      currContent.remove();
-      loadContactPage();
-    }
-  });
+  contactLink.addEventListener('click', switchContents);
   nav.appendChild(contactLink);
 
   document.body.prepend(nav);
+}
+
+function switchContents(e) {
+  const contentToShow = e.target.dataset.name;
+  const currContent = document.getElementById('content').firstChild;
+  if (currContent.id !== contentToShow) {
+    currContent.remove();
+    if (contentToShow == 'home-content') {
+      loadHomePage();
+    }
+    else if (contentToShow == 'menu-content') {
+      loadMenuPage();
+    }
+    else {
+      loadContactPage();
+    }
+  }
 }
