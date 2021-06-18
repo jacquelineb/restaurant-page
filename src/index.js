977aff81fc11cedc37845671cf66f06dadd6f160
jacquelineb <jacquelinesotheremail@gmail.com>
2021-06-17 17:23:20 -0700

    Added logic for switching contents of the page depending on what nav link is clicked

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+import { loadContactPage } from './contact.js';
 import { loadHomePage } from './home.js';
+import { loadMenuPage } from './menu.js';
 
 createNavBar();
 loadHomePage();
@@ -9,21 +11,39 @@ function createNavBar() {
   homeLink.textContent = 'HOME';
   homeLink.href = '#';
   homeLink.addEventListener('click', () => {
-    //const homeContent = document.getElementById('home-content');
-    // get the child of #content
-    // if this child is not homeContent, remove it and load homeContent
-    // if this child is homeContent, don't need to do anything
+    const homeContent = document.getElementById('home-content');
+    const currContent = document.getElementById('content').firstChild;
+    if (currContent !== homeContent) {
+      currContent.remove();
+      loadHomePage();
+    }
   });
   nav.appendChild(homeLink);
 
   const menuLink = document.createElement('a');
   menuLink.textContent = 'MENU';
   menuLink.href = '#';
+  menuLink.addEventListener('click', () => {
+    const menuContent = document.getElementById('menu-content');
+    const currContent = document.getElementById('content').firstChild;
+    if (currContent !== menuContent) {
+      currContent.remove();
+      loadMenuPage();
+    }
+  });
   nav.appendChild(menuLink);
 
   const contactLink = document.createElement('a');
   contactLink.textContent = 'CONTACT';
   contactLink.href = '#';
+  contactLink.addEventListener('click', () => {
+    const contactContent = document.getElementById('contact-content');
+    const currContent = document.getElementById('content').firstChild;
+    if (currContent !== contactContent) {
+      currContent.remove();
+      loadContactPage();
+    }
+  });
   nav.appendChild(contactLink);
 
   document.body.prepend(nav);
