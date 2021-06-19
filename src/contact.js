d74df494475f3f3d992bb42a3c3b95d208f2170a
jacquelineb <jacquelinesotheremail@gmail.com>
2021-06-18 18:58:47 -0700

    Added content to contact page

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,11 @@
+import LocationImg from './location.png'
+
+const contactInfo = {
+  Phone: '555-555-5556',
+  Location: '582010 North Astera, New World',
+  Email: 'thecanteen@realemail.com',
+}
+
 function loadContactContent() {
   const contactContent = document.createElement('div');
   contactContent.setAttribute('id', 'contact-content');
@@ -5,6 +13,16 @@ function loadContactContent() {
   mainHeader.textContent = 'Contact';
   contactContent.appendChild(mainHeader);
   document.getElementById('content').appendChild(contactContent);
+
+  for (const data in contactInfo) {
+    const info = document.createElement('p');
+    info.textContent = `${data}: ${contactInfo[data]}`;
+    contactContent.appendChild(info);
+  }
+
+  const map = new Image();
+  map.src = LocationImg;
+  contactContent.appendChild(map);
 }
 
 export { loadContactContent };
